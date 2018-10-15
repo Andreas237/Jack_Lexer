62767287c405199185de8774b09dd1ad441e106f
Andreas237 <silenta237@gmail.com>
2018-10-15 13:11:43 -0600

    Added Keywords class for comparison against input.  Will branch FAs to based on input bytes

--- src/main/java/com/jackanalyzer/JackAnalyzer.java
+++ src/main/java/com/jackanalyzer/JackAnalyzer.java
@@ -3,24 +3,27 @@ package com.jackanalyzer;
 
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.InputStream;
 import java.nio.file.Files;
+import java.util.HashMap;
 
 // Autogen functions
 import lombok.Data;
 import lombok.Getter;
 
 @Data
-public class JackAnalyzer {
+public class JackAnalyzer extends Keywords {
 
 	
-	private String inFileName;
-	@Getter private FileReader inJack;
-	@Getter private FileWriter outFile;
+	private String inFileName;			// File name for writing
+	@Getter private FileReader inJack;	// File to read JACK from
+	@Getter private FileWriter outFile;	// File to write output to
+	private static HashMap CategoryMap; 		// type <byte, String>
 	
 	
 	
@@ -49,11 +52,20 @@ public class JackAnalyzer {
 		// set the inFileName
 		setInFileName(fin.replace(".jack", "") );
 		
+		// Delete existing output file if it exists
+		removeExistingOutputFile();
+		
 		// Create a filewriter for output
-		setOutFilePointer();
+		this.outFile = setOutFilePointer();
 		
 		// Call the Compilation Engine
-		
+		/*
+		 * Can't use regex/substrings
+		 * Instead use byte ops.
+		 * Can I make a map of {(byte:char),(keyword)} and use same read
+		 * and classify method as I did at CSUEB?
+		 * FA Sim -> first read, then determine states at each KW
+		 */
 		
 		
 		
@@ -81,25 +93,51 @@ public class JackAnalyzer {
 	
 	
 	/**
+	 * Delete output file if it exists.  Log to console if it 
+	 */
+	private void removeExistingOutputFile(){
+		
+		File f = new File(this.inFileName + ".xml");
+		if( f.exists() ){
+			if( !f.delete() )
+				System.out.println("WARNING: Could not delete " + this.inFileName + ".xml");
+		}// end if( f.exists() )
+		else
+			System.out.println("WARNING: " + f.getName() + " DOES NOT exists.");
+		
+	}// end private void removeExistingOutputFile() 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
 	 * Open an input file, throw error if no file given
 	 */
 	public FileReader setInFilePointer(String fin) {
-		FileReader fr;
+		FileReader fr = null;
 		
-		if( !fin.endsWith(".jack") )
+		if( !fin.endsWith(".jack") ){
 			System.out.println("ERROR: Input file must be of type .jack");
+			System.exit(1);
+		}
 		else
 			System.out.println("Input file is " + fin);
 		
 		
 		try{
 			fr = new FileReader(fin) ;
-			return fr;
 		}// end try
 		catch (FileNotFoundException e){
 			System.out.println("ERROR: " + fin + "not found");
+			System.exit(1);
 		}//end catch
-		return null;
+		return fr;
 	} // end public FileReader getFilePointer(String fin)
 	
 	
@@ -114,17 +152,20 @@ public class JackAnalyzer {
 	/**
 	 * Set the output file as specified in Chapter 10 of Nisan and Schoken 
 	 */
-	protected void setOutFilePointer(){
+	protected FileWriter setOutFilePointer(){
 		FileWriter fw = null;
 		try{
 			fw = new FileWriter( getInFileName() + ".xml" );
+			return fw;
 		}
 		catch(IOException e) {
 			System.err.println(" Something wrong in setOutFilePointer");
 			System.err.println(e);
 		}
 		
-		this.outFile = fw;
+		return fw;
 	}// end protected void setOutFilePointer()
+	
+	
 
 }// end class com.jackanalyzer
---
+++ src/main/java/com/jackanalyzer/Keywords.java
@@ -0,0 +1,230 @@
+package com.jackanalyzer;
+
+
+
+
+
+import lombok.Data;
+import lombok.Getter;
+import java.util.HashMap;
+
+
+
+/**
+ * @author andreasslovacek
+ * This class generates the byte byterings for every keyword
+ */
+@Data
+class Keywords {
+	
+	@Getter
+	private HashMap Categories ;
+	
+	
+	// Setup the hashmap of <BytePatter, KeywordName>  
+	Keywords(){
+		
+		Categories = new HashMap<Byte[],String>();
+		Byte[] byter ;
+		
+		
+		
+		//==== KW_CONST ==== 
+		
+		//	true
+		byter = new Byte[] 	{'t','r','u','e'};
+		Categories.put(byter, "KW_CONST");
+		
+		// false
+		byter = new Byte[] 	{'f','a','l','s','e'};
+		Categories.put(byter, "KW_CONST");
+		
+		// null
+		byter = new Byte[] 	{'n','u','l','l'};
+		Categories.put(byter, "KW_CONST");
+		
+		// this
+		byter = new Byte[] 	{'t','h','i','s'};
+		Categories.put(byter, "KW_CONST");
+		
+		
+		
+		//==== KW_TYPE ==== 
+		
+		//int
+		byter 	= new Byte[] 	{'i','n','t'};
+		Categories.put(byter, "KW_TYPE");
+
+		
+		//char
+		byter 		= new Byte[] 	{'c','h','a','r'};
+		Categories.put(byter, "KW_TYPE");
+		
+		//boolean
+		byter 		= new Byte[] 	{'b','o','o','l','e','a','n'};
+		Categories.put(byter, "KW_TYPE");
+		
+		
+		
+		//==== KW_VARDEC ==== 
+		//static
+		byter 		= new Byte[] 	{'s','t','a','t','i','c'};
+		Categories.put(byter, "KW_VARDEC");
+		
+		//static
+		byter 		= new Byte[] 	{'f','i','e','l','d'};
+		Categories.put(byter, "KW_VARDEC");
+		
+		
+		
+		//==== KW_SUBDEC ==== 
+		//conbyteructor
+		byter 		= new Byte[] 	{'c','o','n','s','t','r','u','c','t','o','r'};
+		Categories.put(byter, "KW_SUBDEC");
+		
+		//function
+		byter 		= new Byte[] 	{'f','u','n','c','t','i','o','n'};
+		Categories.put(byter, "KW_SUBDEC");
+		
+		//method
+		byter 		= new Byte[] 	{'m','e','t','h','o','d'};
+		Categories.put(byter, "KW_SUBDEC");
+		
+		
+		
+		
+		//==== KW_VAR ==== 
+		byter 		= new Byte[] 	{'v','a','r'};
+		Categories.put(byter, "KW_VAR");
+
+		
+		
+		//==== KW_VOID ==== 
+		byter 		= new Byte[] 	{'v','o','i','d'};
+		Categories.put(byter, "KW_VAR");
+
+		
+		
+		//==== KW_CLASS ==== 
+		byter 		= new Byte[] 	{'c','l','a','s','s'};
+		Categories.put(byter, "KW_CLASS");
+		
+		
+		
+		//==== KW_LET ==== 
+		byter 		= new Byte[] 	{'l','e','t'};
+		Categories.put(byter, "KW_LET");
+
+		
+		
+		//==== KW_IF ==== 
+		byter 		= new Byte[] 	{'i','f'};
+		Categories.put(byter, "KW_IF");
+		
+		
+		
+		//==== KW_ELSE ==== 
+		byter 		= new Byte[] 	{'e','l','s','e'};
+		Categories.put(byter, "KW_ELSE");
+		
+		
+		
+		//==== KW_WHILE ==== 
+		byter 		= new Byte[] 	{'w','h','i','l','e'};
+		Categories.put(byter, "KW_WHILE");
+		
+		
+		
+		//==== KW_DO ==== 
+		byter 		= new Byte[] 	{'d','o'};
+		Categories.put(byter, "KW_DO");
+		
+		
+		
+		//==== KW_RETURN ==== 
+		byter 		= new Byte[] 	{'r','e','t','u','r','n'};
+		Categories.put(byter, "KW_RETURN");
+		
+		
+		
+		//==== SY_LPAREN ==== 
+		byter 		= new Byte[] 	{'('};
+		Categories.put(byter, "SY_LPAREN");
+		
+		
+		
+		//==== SY_RPAREN ==== 
+		byter 		= new Byte[] 	{')'};
+		Categories.put(byter, "SY_RPAREN");
+		
+		
+		
+		//==== SY_LBRACKET ==== 
+		byter 		= new Byte[] 	{'['};
+		Categories.put(byter, "SY_LBRACKET");
+		
+		
+		
+		//==== SY_RBRACKET ==== 
+		byter 		= new Byte[] 	{']'};
+		Categories.put(byter, "SY_RBRACKET");
+			
+		
+		
+		//==== SY_LBRACE ==== 
+		byter 		= new Byte[] 	{'{'};
+		Categories.put(byter, "SY_LBRACE");
+		
+		
+		
+		//==== SY_RBRACE ==== 
+		byter 		= new Byte[] 	{'}'};
+		Categories.put(byter, "SY_RBRACE");
+		
+		
+		
+		//==== SY_SEMI ==== 
+		byter 		= new Byte[] 	{';'};
+		Categories.put(byter, "SY_SEMI");
+		
+		
+		
+		//==== SY_PERIOD ==== 
+		byter 		= new Byte[] 	{'.'};
+		Categories.put(byter, "SY_PERIOD");
+		
+		
+		
+		//==== SY_COMMA ==== 
+		byter 		= new Byte[] 	{','};
+		Categories.put(byter, "SY_COMMA");
+		
+		
+		
+		//==== SY_EQ ==== 
+		byter 		= new Byte[] 	{'='};
+		Categories.put(byter, "SY_EQ");
+		
+		
+		
+		//==== SY_MINUS ==== 
+		byter 		= new Byte[] 	{'-'};
+		Categories.put(byter, "SY_MINUS");
+		
+		
+		
+		//==== SY_NOT ==== 
+		byter 		= new Byte[] 	{'~'};
+		Categories.put(byter, "SY_NOT");
+		
+		
+		
+		//==== SY_OP ==== 
+		byter 		= new Byte[] 	{'<','>','|','&','/', '*','+'};
+		Categories.put(byter, "SY_OP");
+		
+		
+
+	}// end Keywords() 
+
+}// end class Keywords
